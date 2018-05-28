001ab95ba087dbf1d7674b1c4cfd8493be80f41d
mark <mark@xsnews.nl>
2018-05-28 16:24:22 +0200

    Feature. Initial code

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+use core\Taint;
+use core\Abuse;
+
+class Input {
+    public $val;
+    public $country;
+
+    public function rules() {
+        return array(
+            "val" => array(),
+            "country" => array()
+        );
+    }
+}
+Taint::init();
+$input = Taint::get(new Input());
+if (is_array($input)) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 400 Invalid Input', true, 400);
+    echo "Invalid input.";
+    error_log("xsreg::lookup invinput=" . print_r($input, true));
+    exit;
+}
+Abuse::incr($_SERVER["REMOTE_ADDR"], 40);
+
+    // http://stackoverflow.com/questions/12082507/php-most-lightweight-psr-0-compliant-autoloader
+    spl_autoload_register(function($c){@include preg_replace('#\\\|_(?!.+\\\)#','/',$c).'.php';});
+
+        $input->val = preg_replace("/[^0-9]/", "", $input->val);
+
+        // Lookup
+        try {
+            $util = \libphonenumber\PhoneNumberUtil::getInstance();
+            $proto = $util->parse($input->val, $input->country);
+            if (! $util->isValidNumber($proto)) {
+                throw new \Exception("Invalid phonenumber=" . $input->val . " for country=" . $input->country);
+            }
+            $res = $util->format($proto, \libphonenumber\PhoneNumberFormat::E164);
+            $res = str_replace("+", "", $res);
+
+        } catch (\Exception $e) {
+            //error_log("lookup: phone err=" . $e->getMessage());
+            echo json_encode(array(
+                "ok" => false,
+                "msg" => "Invalid phone number"
+            ));
+            exit;
+        }
+
